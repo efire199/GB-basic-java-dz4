33765916477d49112ccf96ee6c94753150d4cf81
efire199 <efire199@gmail.com>
2021-08-30 18:01:10 +0300

    last commit

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,101 @@
-package PACKAGE_NAME;
+import java.util.Random;
+import java.util.Scanner;
 
-public class enemy {
+public class Enemy {
+    private int healthPoint;
+    private int attackPoint;
+    private int positionX;
+    private int positionY;
+    private String name;
+    private static final int MAXHEALTHPOINT = 20;
+    private static final int MINHEALTHPOINT = 20;
+    private static final int MAXATTACKPOINT = 10;
+    private static final int MINATTACKPOINT = 5;
+    private String status = "Live";
+
+    private static Map map;
+    private static Map enemyMap;
+    private Scanner scanner = new Scanner(System.in);
+    private Random random = new Random();
+
+    public Enemy(String name, Map map) {
+        this.map = map;
+        this.name= name;
+        this.enemyMap = map.getEnemyMap();
+        this.  healthPoint = MINHEALTHPOINT + random.nextInt(MAXHEALTHPOINT + MINHEALTHPOINT + 1);
+        attackPoint = MINATTACKPOINT + random.nextInt(MAXATTACKPOINT + MINATTACKPOINT + 1);
+        do {
+            positionX = random.nextInt(Map.getSizeX());
+            positionY = random.nextInt(Map.getSizeY());
+        } while (enemyMap.getMap()[positionX][positionY]== 'E');
+        enemyMap.enemySpawn(positionX,positionY);
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public int getAttackPoint() {
+        return attackPoint;
+    }
+
+    public void setAttackPoint(int attackPoint) {
+        this.attackPoint = attackPoint;
+    }
+
+    public int getHealthPoint() {
+        return healthPoint;
+    }
+
+    public void setHealthPoint(int healthPoint) {
+        this.healthPoint = healthPoint;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void enemyAttack(Player player){
+        int attackHP = 0;
+        int attackAP = 0;
+        //Направление атаки
+        int  atackVector = random.nextInt(4);
+        switch (atackVector){
+            case 1:
+                if (random.nextInt(100)>50){
+                    attackHP = attackPoint * 2;
+                    System.out.println(name + " ненес урона HP - " + attackHP);
+                }
+                break;
+            case 2:
+                attackHP = attackPoint;
+                System.out.println(name + " ненес урона HP - " + attackHP);
+                break;
+            case 3:
+                if (random.nextInt(100)>20){
+                    attackHP = attackPoint * 80 / 100;
+                    attackAP = player.getAttackPoint()*3/4;
+                    System.out.println(name + " ненес урона HP - " + attackHP + " и ненес урона AP - " + attackAP);
+                }
+                break;
+            default:
+                System.out.println(name + "промахнулся");
+        }
+        player.setAttackPoint(player.getAttackPoint() - attackAP);
+        player.setHealthPoint(player.getHealthPoint() - attackHP);
+        if (player.getHealthPoint() <= 0){
+            player.setStatus("Dead");
+            System.out.println("Player is dead");
+        }
+    }
+    //встреча с плеером
+
 }
--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,97 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
-public class map {
+public class Map {
+    private static int minSize;
+    private static int maxSize;
+    private static int sizeX;
+    private static int sizeY;
+
+    private static final char EMPTYCELL = '_';
+    private static final char ENEMY = 'E';
+    private static final char PLAYER = '*';
+    private static final char OPENCELS = 'X';
+    private Random random = new Random();
+    private char[][] map;
+    private Map enemyMap;
+
+
+    public static int getSizeX() {
+        return sizeX;
+    }
+
+    public static int getSizeY() {
+        return sizeY;
+    }
+
+    public char[][] getMap() {
+        return map;
+    }
+
+    public Map getEnemyMap() {
+        return enemyMap;
+    }
+
+    public Map(int minSize, int maxSize) {
+        Map.minSize = minSize;
+        Map.maxSize = maxSize;
+        sizeX = minSize + random.nextInt(maxSize - minSize + 1);
+        sizeY = minSize + random.nextInt(maxSize - minSize + 1);
+        map = new char[sizeX][sizeY];
+        enemyMap = new Map(this);
+        for (int i = 0; i < sizeX; i++){
+            for (int j = 0; j < sizeY; j++){
+                map[i][j]=EMPTYCELL;
+            }
+        }
+    }
+
+    /*public Map(int size) {
+        map = new char[size][size];
+    }*/
+
+    public Map(Map map) {
+        this.map = new char[Map.sizeX][Map.sizeY];
+
+    }
+
+    public void printMap (){
+        System.out.print("  ");
+        for (int i = 0; i < sizeY; i++){
+            System.out.print(i + 1 + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < sizeX; i++){
+            System.out.print(i+1 + "|");
+            for (int j = 0; j < sizeY; j++){
+                System.out.print(map[i][j]);
+                System.out.print('|');
+            }
+            System.out.println();
+        }
+    }
+
+    public void printEnemyMap (){
+        enemyMap.printMap();
+    }
+
+    public  void setPlayerPosition(int x, int y, int lx, int ly){
+        map[lx][ly] = OPENCELS;
+        map[x][y] = PLAYER;
+    }
+
+    public static boolean validationMove(int x, int y){
+        if (x > sizeX-1 || y > sizeY-1 || x < 0 || y < 0){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    public void enemySpawn(int x, int y){
+        map[x][y] = ENEMY;
+    }
+
+    public  void deleteEnemy(int x, int y) {
+        enemyMap.map[x][y] = 'X';
+    }
 }
